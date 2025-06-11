3a398b27fa1080ae768b26d9f8d522a39d94995b
Suojala Olavi <suojaola@cubbli24-gold.cs.helsinki.fi>
2025-06-11 19:18:45 +0300

    9.3 exercise

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,9 +1,34 @@
 const calculateBmi = (height: number, weight: number) => {
     if (18.5 <= weight/((height/100)**2) && weight/((height/100)**2) <= 24.9) {
         return 'Normal weight'
+    } else if (weight/((height/100)**2) < 18.5) {
+        return 'Underweight'
+    } else if (weight/((height/100)**2) > 24.9) {
+        return 'Overweight'
     } else {
         return 'dont know'
     }
 };
 
-console.log(calculateBmi(180, 74));
+const parseArguments = (args: string[]): number[] => {
+    if (args.length !== 2) {
+        throw new Error('Not right amount of arguments')
+    }
+   const values = args.map(arg => Number(arg));
+   if (isNaN(values[0]) || isNaN(values[1])) {
+    throw new Error('Provided values were not numbers!')
+   }
+   return values;
+}
+
+try {
+    const values = parseArguments(process.argv.slice(2))
+    console.log(calculateBmi(values[0], values[1])); 
+} catch (error: unknown) {
+    let errorMessage = 'now working';
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
+
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -8,7 +8,7 @@ interface Result {
     average: number;
 }
 
-const calculateExercises = (hours: number[], target: number): Result => {
+const calculateExercises = (target: number, hours: number[]): Result => {
     const periodLength = hours.length;
     const trainingDays = hours.filter(day => day > 0).length;
     const average = hours.reduce((sum, day) => sum + day, 0) / periodLength;
@@ -39,4 +39,25 @@ const calculateExercises = (hours: number[], target: number): Result => {
     };
 };   
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+const modifyArgs = (args: string[]) => {
+    const list = args.map(item => Number(item))
+    if (list.every(item => !isNaN(item))) {
+        const target = list[0]
+        const hours = list.slice(1)
+        return {
+            value1: target,
+            value2: hours
+        }
+    } else throw new Error('items are not numbers')
+}
+
+try {
+    const { value1, value2 } = modifyArgs(process.argv.slice(2));
+    console.log(calculateExercises(value1, value2));
+} catch (error: unknown) {
+    let errorMessage = 'not working';
+    if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+    console.log(errorMessage);
+}
